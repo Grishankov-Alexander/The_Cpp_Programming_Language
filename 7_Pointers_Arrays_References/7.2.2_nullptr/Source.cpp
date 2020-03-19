713e5a179be7a799fb713cba810910b1d9955406
ache2014 <ache2014@gmail.com>
2020-03-19 13:31:33 +0300

    Add examples for section 7.2.2 "nullptr".

---
+++ 7_Pointers_Arrays_References/7.2.2_nullptr/Source.cpp
@@ -0,0 +1,5 @@
+int* pi = nullptr;
+double* pd = nullptr;
+int i = nullptr;  // error: i is not a pointer
+int* x = 0;  // x gets the value nullptr
+int* p = NULL;  // error: can't assign a void* to an int*

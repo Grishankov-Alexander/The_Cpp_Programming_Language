ee2aec7fdf229a1e305eed155aee334f62fb5c09
ache2014 <ache2014@gmail.com>
2020-03-25 16:59:57 +0300

    Add examples for section 9.5.2 "for Statements".

---
+++ 9_Statements/9.5.2_for_Statements/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+void f(int v[], int max)
+{
+	for (int i = 0; i != max; ++i)
+		v[i] = i * i;
+}
+
+for (string s; cin >> s;) v.push_back(s);
+
+for (;;) {  // forever
+	// ...
+}

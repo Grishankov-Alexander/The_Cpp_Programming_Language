9fb845b92862ffa6a8f075b6f91bf010b6177e18
ache2014 <ache2014@gmail.com>
2020-03-26 17:09:44 +0300

    Add examples for section 10.2.4 "Error Handling".

---
+++ 10_Expressions/10.2.4_Error_Handling/Source.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int no_of_errors;
+double error(const string& s)
+{
+	no_of_errors++;
+	cerr << "error: " << s << '\n';
+	return 1;
+}

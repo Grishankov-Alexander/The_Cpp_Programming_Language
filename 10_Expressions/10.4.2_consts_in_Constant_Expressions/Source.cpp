f47374e5f81228944746243c34deeb9288ca9b04
ache2014 <ache2014@gmail.com>
2020-03-27 15:03:58 +0300

    Add examples for section 10.4.2 "consts in Constant Expressions".

---
+++ 10_Expressions/10.4.2_consts_in_Constant_Expressions/Source.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <cmath>
+using namespace std;
+
+
+const int x = 7;
+const string s = "asdf";
+const int y = sqrt(x);
+constexpr int xx = x;  // OK
+constexpr string ss = s;  // error: s is not a constant expression
+constexpr int yy = y;  // error: sqrt(x) is not a constant expression
